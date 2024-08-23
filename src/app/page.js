8cd10a22aa75a2a8a0643c2ff26807b43cca6260
Jenish tailor <111968544+jenishwebdev@users.noreply.github.com>
2024-08-23 17:41:02 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import Home from "@/components/Home";
 
+export const metadata = {
+  title: "Why customisable hampers are the perfect father's day gift",
+  description: "Why customisable hampers are the perfect father's day gift",
+};
+
 export default function HomePage() {
   return <Home />;
 }
